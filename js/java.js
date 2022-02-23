140e0166a23f58af8af428e2ef75765da0d38cc6
jmoore3000 <fender655@gmail.com>
2022-02-23 15:33:44 -0600

    added the banner images 1-3, moved the java.js into a js folder in root, styled the top navigation bar, applied the scripting needed to make the rotating banner.

---
+++ js/java.js
@@ -0,0 +1,12 @@
+$(function() {
+    $('#slideshow img:gt(0)').hide();
+    setInterval(function() {
+    $('#slideshow :first-child')
+    .fadeOut(1000)
+    .next('img')
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+    }, 4000);
+    });
+
